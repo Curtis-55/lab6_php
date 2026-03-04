c22f71474600d8944b94ee1c19ae6a7f56d2c990
Curtis-55 <curtis.palmer@uri.edu>
2026-03-04 13:25:05 -0500

    Add files via upload

--- includes/book.php
+++ includes/book.php
@@ -1,1 +1,61 @@
+<?php
+
+
+class Book {
+   public string $image;
+   public string $title;
+   public string $author;
+   public int $rating;
+   public float $price;
+   public int $quantity;
+
+
+   public function __construct(string $image, string $title, string $author, int $rating, float $price, int $quantity) {
+       $this->image = $image;
+       $this->title = $title;
+       $this->author = $author;
+       $this->rating = $rating;
+       $this->price = $price;
+       $this->quantity = $quantity;
+   }
+
+
+       public function checkStock(): string {
+           if ($this->quantity > 0) {
+               return "In Stock";
+           } else {
+               return "Out of Stock";
+           }
+       }
+
+
+       public function displayRating(): string {
+           $stars = "";
+           for ($i = 0; $i < $this->rating; $i++) {
+               $stars .= "<i class='fa fa-star'></i>";
+           }
+           for ($j = $this->rating; $j < 5; $j++) {
+               $stars .= "<i class='fa fa-star-o'></i>";
+           }
+           return $stars;
+       }
+
+
+       public function buy(): int {
+           if ($this->quantity > 0) {
+               $this->quantity--;
+       }
+           return $this->quantity;
+       }
+}
+
+
+$books = [new Book("images/hunger-games.jpeg", "The Hunger Games", "Suzanne Collins", 5, 14.99, 1),
+         new Book("images/catching-fire.jpeg", "Catching Fire", "Suzanne Collins", 4, 13.99, 3),
+         new Book("images/mockingjay.jpeg", "Mockingjay", "Suzanne Collins", 3, 10.99, 5)];
+
+
+
+
+?>
 
--- index.php
+++ index.php
@@ -1,58 +1,50 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Lab 9: The Bookworm Store</title>
-		<link rel="stylesheet" href="css/bookworm-style.css" />
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	</head>
+   <head>
+       <title>Lab 9: The Bookworm Store</title>
+       <link rel="stylesheet" href="css/bookworm-style.css" />
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
+   </head>
 
-	<body>
-		<header>
-			<h1>THE BOOKWORM STORE</h1>
-			<nav>
-				<a href="index.php">HOME</a>
-			</nav>
-    	</header>
 
-    	<section>
-      		<div>
-		        <h2>The BookWorm Store</h2>
-		        <h4>I'm not a <span class="emphasized">bookworm</span>, I'm an <span class="emphasized">escape artist</span>.</h4>
-  			</div>
+   <body>
+       <header>
+           <h1>THE BOOKWORM STORE</h1>
+           <nav>
+               <a href="index.php">HOME</a>
+           </nav>
+       </header>
 
-  			<h4 class="decorated"><span><span class="emphasized">The Hunger Games</span></span></h4>
 
-	  		<div id="content">
-	  			<div class="row">
+       <section>
+           <?php
+               include "includes/book.php";
+           ?>
+           <div>
+               <h2>The BookWorm Store</h2>
+               <h4>I'm not a <span class="emphasized">bookworm</span>, I'm an <span class="emphasized">escape artist</span>.</h4>
+           </div>
 
-	  				<div class="column">
-	  					<img src="IMAGE" style="width:60%">
-	  					<h5>TITLE</h5>
-	  					<h5>by AUTHOR</h5>
-	  					<!-- SHOW STAR RATINGS -->
-	  					<h5><span class="emphasized">CHECK STOCK</span>
-						<h5>$PRICE</h5>
-	  				</div>
 
-	  				<div class="column">
-	  					<img src="IMAGE" style="width:60%">
-	  					<h5>TITLE</h5>
-	  					<h5>by AUTHOR</h5>
-	  					<!-- SHOW STAR RATINGS -->
-	  					<h5><span class="emphasized">CHECK STOCK</span>
-						<h5>$PRICE</h5>
-	  				</div>
+           <h4 class="decorated"><span><span class="emphasized">The Hunger Games</span></span></h4>
 
-	  				<div class="column">
-	  					<img src="IMAGE" style="width:60%">
-	  					<h5>TITLE</h5>
-	  					<h5>by AUTHOR</h5>
-	  					<!-- SHOW STAR RATINGS -->
-	  					<h5><span class="emphasized">CHECK STOCK</span>
-						<h5>$PRICE</h5>
-	  				</div>
-				</div>
-			</div>
-    	</section>
-	</body>
+
+           <div id="content">
+               <div class="row">
+               <?php foreach ($books as $book): ?>
+                               <div class="column">
+                                   <img src="<?php echo $book->image; ?>" style="width:60%">
+                                   <h5><?php echo $book->title; ?></h5>
+                                   <h5>by <?php echo $book->author; ?></h5>
+                                   <h5><?php echo $book->displayRating(); ?></h5>
+                                   <h5><?php echo $book->checkStock(); ?></h5>
+                                   <h5>$<?php echo $book->price; ?></h5>
+                               </div>
+               <?php endforeach; ?>
+               </div>
+               </div>
+           </div>
+       </section>
+   </body>
 </html>
